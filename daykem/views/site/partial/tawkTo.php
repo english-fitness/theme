831c594153ecf74004eff211432cf077af5ab0da
thphuong <thp.1093@outlook.com>
2015-07-15 15:21:21 +0700

    tawk.to widget

--- daykem/views/site/partial/tawkTo.php
+++ daykem/views/site/partial/tawkTo.php
@@ -4,7 +4,7 @@ var $_Tawk_API={},$_Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/55485902c219960305d74636/default';
+s1.src='https://embed.tawk.to/55a6150d84d307454c0158a3/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
