134e9614001b0b521d82b3cd580951c6e954b6a1
thphuong <thp.1093@outlook.com>
2015-09-29 10:28:35 +0700

    move bootstrap javascript to header

--- daykem/views/admin/partial/footer.php
+++ daykem/views/admin/partial/footer.php
@@ -1,5 +1,4 @@
 <div id="footer">
 
 </div>
-<script src="<?php echo Yii::app()->theme->baseUrl; ?>/bootstrap/js/bootstrap.min.js"></script>
 <script src="<?php echo Yii::app()->theme->baseUrl; ?>/tags/bootstrap-tagsinput.min.js"></script>
